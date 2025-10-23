a8a66d4388aa53392503b3ccc259985cb6eceb54
durgeshceps <101636946+durgeshceps@users.noreply.github.com>
2025-10-23 15:11:04 +0530

    Update StudentAttendanceDetail.java

--- University.Management.System/src/StudentAttendanceDetail.java
+++ University.Management.System/src/StudentAttendanceDetail.java
@@ -1,60 +1,3 @@
-//package university.management.system;
-//
-//
-//
-//import javax.swing.*;
-//import java.sql.ResultSet;
-//import java.sql.*;
-//import javax.swing.*;
-//import java.awt.*;
-//import java.awt.event.*;
-//
-//public class StudentAttendanceDetail extends JFrame implements  {
-//
-//    JTable j1;
-//    JButton b1;
-//    String h[]={"Roll Number","Date Time","First Half","Second Half"};
-//    String d[][]=new String[15][4];
-//    int i=0,j=0;
-//
-//    StudentAttendanceDetail(){
-//        super ("View Student Attendance");
-//        setSize(800,300);
-//        setLocation(450,150);
-//
-//        try{
-//            String q="select * from attendance_student";
-//            conn d1=new conn();
-//            ResultSet rs=c1.s.execteQuery(q);
-//            while(rs.next()){
-//                d[i][j++]=rs.getString("rollno");
-//                d[i][j++]=rs.getString("Date");
-//                d[i][j++]=rs.getString("first");
-//                d[i][j++]=rs.getString("second");
-//                i++;
-//                j=0;
-//
-//            }
-//            j1=new JTable(d,h);
-//
-//        }catch (Exception e){}
-//
-//        b1=new JButton("Print");
-//        add(b1,"South");
-//        JScrollPane s1=new JScrollPane(j1);
-//        add(s1);
-//
-//        b1.addActionListener(this);
-//    }
-//
-//    public void actionPerformed(ActionEvent ae){
-//        try{
-//            j1.print();
-//
-//        }catch (Exception e){}
-//    }
-//    public static void main(String[] args){new StudentAttendanceDetail().setVisible();}
-//}
 
 package university.management.system;
 
@@ -117,3 +60,4 @@ public class StudentAttendanceDetail extends JFrame implements ActionListener {
         new StudentAttendanceDetail().setVisible(true);
     }
 }
+
