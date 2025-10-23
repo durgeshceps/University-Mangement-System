09623201b73a51ea3ee8a546cdea88469070dbcd
durgeshceps <101636946+durgeshceps@users.noreply.github.com>
2025-10-23 15:10:18 +0530

    Update Splash.java

--- University.Management.System/src/Splash.java
+++ University.Management.System/src/Splash.java
@@ -1,63 +1,4 @@
-////part1
-//
-//package university.management.system;
-//
-//import java.awt.*;
-//import javax.swing.*;
-//
-//
-//
-//
-//public class Splash {
-//public static void main(String []  args){
-//    SplashFrame f = new SplashFrame("KG University");
-//    f.setVisible(true);
-//    int i;
-//    int x = 1;
-//    for(i = 2; i<= 600; i += 4, x += 1){
-//        f.setLocation((800 - (( i + x) / 2) ), 500 - (i / 2));
-//        f.setSize(i+3*x, i+x/2);
-//        try {
-//            Thread.sleep(10);
-//        } catch (Exception e){
-//
-//        }
-//    }
-//   }
-//  }
-//
-//   class SplashFrame extends JFrame implements Runnable {
-//       SplashFrame(String s) {
-//           super(s);
-//           setLayout(new FlowLayout());
-//
-//           ImageIcon c1 = new ImageIcon(ClassLoader.getSystemResource("src/icone/slide1.jpg"));
-//           Image i1 = c1.getImage().getScaledInstance(1200, 675, Image.SCALE_DEFAULT);
-//           ImageIcon i2 = new ImageIcon(i1);
-//
-//           JLabel m1 = new JLabel(i2);
-//           add(m1);
-//           Thread t1 = new Thread(this);
-//           t1.start();
-//
-//       }
-//
-//
-//       public void run() {
-//           try {
-//               Thread.sleep(7000);
-//               this.setVisible(false);
-//
-//
-//               Login f1 = new Login();
-//           } catch (Exception e) {
-//               e.printStackTrace();
-//           }
-//       }
-//   }
-//
-//
-//
+
 
 package university.management.system;
 
@@ -106,3 +47,4 @@ class SplashFrame extends JFrame {
         }).start();
     }
 }
+
